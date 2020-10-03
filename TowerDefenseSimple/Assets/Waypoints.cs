9b54e19f0f813323f1684cf80208d0e3c73d1a84
Việt Hoàng Cai <cvhoangpt@gmail.com>
2020-10-03 20:53:20 +0700

    Add waypoints and enemy

---
+++ TowerDefenseSimple/Assets/Waypoints.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Waypoints : MonoBehaviour
+{
+    public static Transform[] points; //To create point mark one enemy way
+
+    void Awake()
+    {
+        points = new Transform[transform.childCount];
+        for (int i = 0; i < points.Length; i++)
+        {
+            points[i] = transform.GetChild(i);
+        }
+    }
+}
