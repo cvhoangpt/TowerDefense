43434fd20447cb9ffb56f02aa1b5414651451b7f
Việt Hoàng Cai <cvhoangpt@gmail.com>
2020-11-12 10:03:52 +0700

    add laser slowing effect and update gitignore

--- TowerDefenseSimple/Assets/Scripts/Enemy.cs
+++ TowerDefenseSimple/Assets/Scripts/Enemy.cs
@@ -2,20 +2,22 @@
 using UnityEngine;
 public class Enemy : MonoBehaviour
 {
+    public float startSpeed = 10f;
+
+    [HideInInspector]
     public float speed = 10f;
-    public int health = 100;
-    public int value = 50;
+
+    public float health = 100;
+
+    public int worth = 50;
 
     public GameObject deathEffect;
 
-    private Transform target;
-    private int wayPointIndex = 0;
-
     void Start()
     {
-        target = Waypoints.points[0];
+        speed = startSpeed;    
     }
-    public void TakeDamage(int amount)
+    public void TakeDamage(float amount)
     {
         health -= amount;
         if (health <= 0)
@@ -23,41 +25,21 @@ public class Enemy : MonoBehaviour
             Die();
         }
     }
+
+    //percentage: pct
+    public void Slow(float pct)
+    {
+        speed = startSpeed * (1f - pct);
+    }    
+
     void Die()
     {
-        PlayerStats.Money += value;
+        PlayerStats.Money += worth;
         GameObject effect = (GameObject) Instantiate(deathEffect, transform.position, Quaternion.identity);
         Destroy(effect, 5f);
 
-        Debug.Log("Error hapen!!");
+        //Debug.Log("Error hapen!!");
         
-        Destroy(gameObject);
-    }
-
-    void Update()
-    {
-        Vector3 dir = target.position - transform.position;
-        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World); 
-        if (Vector3.Distance(transform.position, target.position) <= 0.2f)
-        {
-            GetNextWayPoint();
-        }
-    }
-
-    void GetNextWayPoint()
-    {
-        if (wayPointIndex >= Waypoints.points.Length - 1)
-        {
-            EndPath();
-            return;
-        }
-        wayPointIndex++;
-        target = Waypoints.points[wayPointIndex];
-    }
-
-    void EndPath()
-    {
-        PlayerStats.Lives--;
         Destroy(gameObject);
     }
 
---
+++ TowerDefenseSimple/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(Enemy))]
+public class EnemyMovement : MonoBehaviour
+{
+    private Transform target;
+    private int wayPointIndex = 0;
+
+    private Enemy enemy;
+    void Start()
+    {
+        enemy = GetComponent<Enemy>();
+
+        target = Waypoints.points[0];
+    }
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * enemy.speed * Time.deltaTime, Space.World);
+        if (Vector3.Distance(transform.position, target.position) <= 0.2f)
+        {
+            GetNextWayPoint();
+        }
+
+        enemy.speed = enemy.startSpeed;
+    }
+
+    void GetNextWayPoint()
+    {
+        if (wayPointIndex >= Waypoints.points.Length - 1)
+        {
+            EndPath();
+            return;
+        }
+        wayPointIndex++;
+        target = Waypoints.points[wayPointIndex];
+    }
+
+    void EndPath()
+    {
+        PlayerStats.Lives--;
+        Destroy(gameObject);
+    }
+}
