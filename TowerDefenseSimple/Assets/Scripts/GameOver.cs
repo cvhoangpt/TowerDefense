f85e0783ec638890057fa35dd434ceac14572c6f
Cai Viet Hoang <cvhoangpt@gmail.com>
2020-12-09 20:07:05 +0700

    Big Update: Level GUI #2

--- TowerDefenseSimple/Assets/Scripts/GameOver.cs
+++ TowerDefenseSimple/Assets/Scripts/GameOver.cs
@@ -5,7 +5,7 @@ public class GameOver : MonoBehaviour
 {
     public string menuSceneName = "MainMenu";
     public SceneFader sceneFader;
-    
+
     public void Retry()
     {
         sceneFader.FadeTo(SceneManager.GetActiveScene().name);
