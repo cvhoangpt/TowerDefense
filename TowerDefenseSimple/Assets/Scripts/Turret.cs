9f4df0f255d391ff3bf00a709f65139a2a6db3a2
Cai Viet Hoang <cvhoangpt@gmail.com>
2020-12-09 10:54:05 +0700

    Update: GUI

--- TowerDefenseSimple/Assets/Scripts/Turret.cs
+++ TowerDefenseSimple/Assets/Scripts/Turret.cs
@@ -137,7 +137,7 @@ public class Turret : MonoBehaviour
         impactEffect.transform.rotation = Quaternion.LookRotation(dir);
 
         // audioSource.clip = laserSound;
-        AudioSource.PlayClipAtPoint(laserSound, 0.8f * Camera.main.transform.position + 0.2f * transform.position, volume);
+        AudioSource.PlayClipAtPoint(laserSound, transform.position, volume);
         //Destroy(laserSound);
 
     }
